808a249524e94d95770a428d90295696cf660915
Eben <eben.lowe@gmail.com>
2013-12-16 17:21:50 -0500

    Successfully wrote method to limit number of Sales displayed.

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,4 +2,20 @@ class Sale < ActiveRecord::Base
   belongs_to :employee
   belongs_to :product
   belongs_to :customer
+
+  def last_thirteen_months?
+    require 'date'
+    date = Date.today - 395
+    sale_date > date
+  end
+
+  def self.limited
+    results = []
+    self.all.order_by(:sale_date).each do |sale|
+      results << sale if sale.last_thirteen_months?
+    end
+    results
+  end
+
+
 end
