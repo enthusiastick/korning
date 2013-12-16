00886e049cd7b8ed27dc805a33eae568cf955b70
Eben <eben.lowe@gmail.com>
2013-12-16 14:51:01 -0500

    Added product id to the sales table.

--- db/migrate/20131216193740_normalize_sales3.rb
+++ db/migrate/20131216193740_normalize_sales3.rb
@@ -1,4 +1,12 @@
 class NormalizeSales3 < ActiveRecord::Migration
   def change
+    add_column :sales, :product_id, :integer
+
+    Sale.all.each do |sale|
+      id_number = Product.select(:id).where('name = ?', sale.product_name).pluck(:id)[0]
+      sale.product_id = id_number
+      sale.save
+    end
+
   end
 end
