505425c735a214fb969f65732d9853ef9fe1f12d
Eben <eben.lowe@gmail.com>
2013-12-16 17:02:44 -0500

    Mistake in the employee seed file (email not saved.)

--- db/migrate/20131216163657_seed_employees.rb
+++ db/migrate/20131216163657_seed_employees.rb
@@ -2,6 +2,7 @@ class SeedEmployees < ActiveRecord::Migration
   def change
     Sale.all.each do |sale|
       Employee.find_or_create_by email: sale.employee.split[2].delete('(').delete(')') do |employee|
+        employee.email = sale.employee.split[2].delete('(').delete(')')
         employee.firstname = sale.employee.split[0]
         employee.lastname = sale.employee.split[1]
         employee.save
