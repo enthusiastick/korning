21dd4bbf7f2c783e2d74901f59044f22b54bdd98
Eben <eben.lowe@gmail.com>
2013-12-16 12:53:05 -0500

    Seeded employee table.

--- db/migrate/20131216163657_seed_employee.rb
+++ db/migrate/20131216163657_seed_employee.rb
@@ -1,10 +1,11 @@
 class SeedEmployee < ActiveRecord::Migration
   def change
     Sale.all.each do |sale|
-      Employee.find_or_create_by email: sale.employee.split[2].delete('(').delete(')')
-      Employee.firstname = sale.employee.split[0]
-      Employee.lastname = sale.employee.split[1]
-      Employee.save
+      Employee.find_or_create_by email: sale.employee.split[2].delete('(').delete(')') do |employee|
+        employee.firstname = sale.employee.split[0]
+        employee.lastname = sale.employee.split[1]
+        employee.save
+      end
     end
   end
 end
