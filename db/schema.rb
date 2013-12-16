da43c9e4c5f79066366cc0ee844d2d1a0d3f9ac4
Eben <eben.lowe@gmail.com>
2013-12-16 15:35:34 -0500

    Added customers view (and controller and route.)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216193740) do
+ActiveRecord::Schema.define(version: 20131216195648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,9 +39,6 @@ ActiveRecord::Schema.define(version: 20131216193740) do
   end
 
   create_table "sales", force: true do |t|
-    t.string   "employee"
-    t.string   "customer_and_account_no"
-    t.string   "product_name"
     t.date     "sale_date"
     t.float    "sale_amount"
     t.integer  "units_sold"
