2eb5434b4f4e2480a63e8e9b9a13e9578eecfb3c
Eben <eben.lowe@gmail.com>
2013-12-16 15:56:55 -0500

    Commented out associations in an attempt to salvage database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216195648) do
+ActiveRecord::Schema.define(version: 20131216191326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +32,6 @@ ActiveRecord::Schema.define(version: 20131216195648) do
     t.datetime "updated_at"
   end
 
-  create_table "products", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "sales", force: true do |t|
     t.date     "sale_date"
     t.float    "sale_amount"
@@ -48,7 +42,9 @@ ActiveRecord::Schema.define(version: 20131216195648) do
     t.datetime "updated_at"
     t.integer  "employee_id"
     t.integer  "customer_id"
-    t.integer  "product_id"
+    t.string   "product_name"
+    t.string   "customer_and_account_no"
+    t.string   "employee"
   end
 
 end
