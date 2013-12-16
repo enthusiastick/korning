af5a6cc5a61de81002e1b02d03919f4e000d6e09
Eben <eben.lowe@gmail.com>
2013-12-16 14:26:28 -0500

    Created products table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216191326) do
+ActiveRecord::Schema.define(version: 20131216192204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20131216191326) do
     t.string   "firstname"
     t.string   "lastname"
     t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
