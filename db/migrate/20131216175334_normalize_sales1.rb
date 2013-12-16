fc4ed857e8af497b30abaa58876eaf3a2ab23dda
Eben <eben.lowe@gmail.com>
2013-12-16 13:40:52 -0500

    Added employee_id column to sale.

---
+++ db/migrate/20131216175334_normalize_sales1.rb
@@ -0,0 +1,12 @@
+class NormalizeSales1 < ActiveRecord::Migration
+  def change
+    add_column :sales, :employee_id, :integer
+
+    Sale.all.each do |sale|
+      id_number = Employee.select(:id).where('email = ?', sale.employee.split[2].delete('(').delete(')')).pluck(:id)[0]
+      sale.employee_id = id_number
+      sale.save
+    end
+
+  end
+end
